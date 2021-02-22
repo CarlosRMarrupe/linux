7acd67b730e09750ac0b0622d2ed844f6357d7f4
CarlosRMarrupe <c.rmarrupe@gmail.com>
2021-02-22 09:45:10 +0100

    Update install-docker.sh

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #########################################################################
 # Carlos Ramírez - 22/02/2021
-# Install docker-ce on CentOS / RHEL
+# Install docker-ce and docker-compose on CentOS / RHEL
 #########################################################################
 engine=docker                                                           ;
 repo=https://download.docker.com/linux/centos/docker-ce.repo		;
@@ -26,4 +26,11 @@ done                                                                    ;
 #########################################################################
 sudo systemctl restart ${engine}                 			;
 sudo systemctl enable  ${engine}  	                                ;
+#########################################################################
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" \
+-o /usr/local/bin/docker-compose                                        ;
+sudo chmod +x /usr/local/bin/docker-compose                             ;
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose        ;
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.28.4/contrib/completion/bash/docker-compose \
+-o /etc/bash_completion.d/docker-compose                                ;
 #########################################################################
