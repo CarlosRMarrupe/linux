04fab885a8eba552f71a736ed8e312cd2ee5afaf
CarlosRMarrupe <c.rmarrupe@gmail.com>
2021-02-22 09:32:51 +0100

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#########################################################################
+# Carlos Ramírez - 22/02/2021
+# Install docker-ce on CentOS / RHEL
+#########################################################################
+engine=docker                                                           ;
+repo=https://download.docker.com/linux/centos/docker-ce.repo		;
+#########################################################################
+sudo yum update -y                                               ;
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2	;
+sudo yum-config-manager --add-repo ${repo}                       ;
+#########################################################################
+
+for package in                                                          \
+        ${engine}-ce                                                    \
+                                                                        ;
+do                                                                      \
+        while true                                                      ;
+        do                                                              \
+                yum list installed ${package}                           \
+                &&                                                      \
+                break                                                   ;
+                sudo yum install -y ${package}                          ;
+                sleep 10                                                ;
+        done                                                            ;
+done                                                                    ;
+#########################################################################
+sudo systemctl restart ${engine}		                              			;
+sudo systemctl enable --now ${engine}	                                  ;
+#########################################################################
