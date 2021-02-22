c3f88a300492b2898cac2df5168fbb0f20375c3b
CarlosRMarrupe <c.rmarrupe@gmail.com>
2021-02-22 09:46:41 +0100

    Update install-docker.sh

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -27,10 +27,3 @@ done                                                                    ;
 sudo systemctl restart ${engine}                 			;
 sudo systemctl enable  ${engine}  	                                ;
 #########################################################################
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" \
--o /usr/local/bin/docker-compose                                        ;
-sudo chmod +x /usr/local/bin/docker-compose                             ;
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose        ;
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.28.4/contrib/completion/bash/docker-compose \
--o /etc/bash_completion.d/docker-compose                                ;
-#########################################################################
